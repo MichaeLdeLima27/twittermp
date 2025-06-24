37d74810740fba0846f2b5fedb094ceb82c343fd
MichelPianaro <teconhecer27@gmail.com>
2025-06-23 21:10:37 -0300

    commit final

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://vitorbri.pythonanywhere.com/api/',
+    baseUrl: 'https://michaelmp.pythonanywhere.com/api/',
     prepareHeaders: (headers, { endpoint }) => {
       const token = localStorage.getItem('access_token')
       if (token && endpoint !== 'register') {
