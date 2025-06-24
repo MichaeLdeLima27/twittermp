b680202d74dffe5c3fe93d90119f0bc3d594d644
MichelPianaro <teconhecer27@gmail.com>
2025-06-23 21:10:23 -0300

    commit final

--- twitter_back/settings.py
+++ twitter_back/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-3i96pmw183(#@fes!fwnf)ex(&3mub-6ij*1v4&!*lwr%3mljv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'VitorBri.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'michaelmp.pythonanywhere.com']
 
 # Application definition
 
